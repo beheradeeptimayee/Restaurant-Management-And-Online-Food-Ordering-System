c6d8d0443426e3cb6e8f1f28408793b16a032706
Deepti Mayee Behera <deeptimayee2711@gmail.com>
2026-03-04 13:10:52 +0530

    Update settings.py

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -17,8 +17,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 TEMPLATE_DIR=os.path.join(BASE_DIR,'templates')
 
-STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -122,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
